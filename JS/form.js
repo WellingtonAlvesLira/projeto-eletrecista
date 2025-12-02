a3b79e235c4dbc95df9b87adf31641106b44b108
Wellington Alves <63911446+WellingtonAlvesLira@users.noreply.github.com>
2025-12-02 15:00:06 -0400

    adicionando a menssagem de feedback ao enviar email

--- JS/form.js
+++ JS/form.js
@@ -1,27 +1,29 @@
+(function(){
+emailjs.init({
+publicKey: "B1z8GVT3zwB-fq_NK",
+});
+})();
+window.onload = function() {
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+event.preventDefault(); // Impede o envio tradicional do formulário
+// Substitua pelos seus Service ID, Template ID e Public Key
+const serviceID = 'service_b9vl69h';
+const templateID = 'template_8caws8j';
+// emailjs.sendForm(serviceID, templateID, form, publicKey)
+emailjs.sendForm(serviceID, templateID, this)
+.then(() => {
+document.getElementById('feedbackEmail').textContent = "Enviando o seu E-MAIL..."
+setTimeout(() => {
+document.getElementById('feedbackEmail').textContent = "E-MAIL enviado com sucesso";
+setTimeout(() => {
+document.getElementById('feedbackEmail').textContent = "";
+}, 3000);
+}, 3000);
 
-   (function(){
-      emailjs.init({
-        publicKey: "B1z8GVT3zwB-fq_NK",
-      });
-   })();
 
-   
-   window.onload = function() {
-       document.getElementById('contact-form').addEventListener('submit', function(event) {
-           event.preventDefault(); // Impede o envio tradicional do formulário
-
-           // Substitua pelos seus Service ID, Template ID e Public Key
-           const serviceID = 'service_b9vl69h';
-           const templateID = 'template_8caws8j';
-
-           // emailjs.sendForm(serviceID, templateID, form, publicKey)
-           emailjs.sendForm(serviceID, templateID, this)
-               .then(() => {
-                   alert('E-mail enviado com sucesso!');
-                   // Opcional: resetar o formulário
-                   document.getElementById('contact-form').reset();
-               }, (err) => {
-                   alert('Falha ao enviar e-mail:', JSON.stringify(err));
-               });
-       });
-   }
+document.getElementById('contact-form').reset();
+}, (err) => {
+alert('Falha ao enviar e-mail:', JSON.stringify(err));
+});
+});
+}
